25fdec69bcd7769806226fe878742c3ec0785096
Mikkamae3d <mikkamae04@gmail.com>
2024-11-04 08:12:23 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,15 @@
-<?php
+<php
+$servername = "localhost";
+$username = "email";
+$username = "username";
+$password = "password";
 
+// Create connection
+$conn = mysqli_connect($servername, $email, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
